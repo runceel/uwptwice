1dadbe92d64400b5e05f4f44772aa6277f33fb05
Kazuki Ota <kaota@microsoft.com>
2017-10-22 18:11:55 +0900

    プロジェクト ファイルを追加します。

---
+++ App4/App4/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new MainPageViewModel();
+        }
+    }
+
+    public class MainPageViewModel
+    {
+        public Command<string> SelectedCommand { get; }
+
+        public IEnumerable<string> Items { get; } = Enumerable.Range(1, 10).Select(x => x.ToString()).ToArray();
+
+        public MainPageViewModel()
+        {
+            this.SelectedCommand = new Command<string>(x => Debug.WriteLine(x));
+        }
+    }
+}
